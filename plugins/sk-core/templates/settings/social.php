43eed71392d41b2c8f3d982801ed8d08366867a8
warioishere <mario.hofmann@yourdevice.ch>
2026-04-18 11:55:43 +0200

    Settings-Refactor Phase 2: social.php auf sk_form_input + sk-settings-field umgestellt (Submit-Button bleibt unverändert)

--- plugins/sk-core/templates/settings/social.php
+++ plugins/sk-core/templates/settings/social.php
@@ -1,42 +1,41 @@
 <?php
 /**
+ * SK Settings — Social Profile Form.
+ *
+ * Available vars:
+ *   @var int    $current_user
+ *   @var array  $profile_info
+ *   @var array  $social_fields  [key => ['title' => …, 'icon' => fa-icon-name]]
  */
-do_action( 'sk_profile_settings_before_form', $current_user, $profile_info ); ?>
 
-<form method="post" id="profile-form"  action="" class="sk-form-horizontal"><?php ///settings-form ?>
+defined( 'ABSPATH' ) || exit;
 
-    <?php wp_nonce_field( 'sk_profile_settings_nonce' ); ?>
+do_action( 'sk_profile_settings_before_form', $current_user, $profile_info );
+?>
 
-    <?php foreach( $social_fields as $key => $field ) { ?>
-        <div class="sk-form-group">
-            <label class="sk-w3 sk-control-label"><?php echo $field['title']; ?></label>
+<form method="post" id="profile-form" action="" class="sk-settings-form">
+	<?php wp_nonce_field( 'sk_profile_settings_nonce' ); ?>
 
-            <div class="sk-w5">
-                <div class="sk-form-group">
-                    <div class="sk-input-group">
-                        <span class="sk-input-group-addon"><i class="fab fa-<?php echo isset( $field['icon'] ) ? $field['icon'] : ''; ?>"></i></span>
-                        <input id="settings[social][<?php echo $key; ?>]" value="<?php echo isset( $profile_info['social'][ $key ] ) ? esc_url( $profile_info['social'][ $key ] ) : ''; ?>" name="settings[social][<?php echo $key; ?>]" class="sk-form-control" placeholder="http://" type="url">
-                    </div>
-                </div>
-            </div>
-        </div>
-    <?php } ?>
+	<?php foreach ( $social_fields as $key => $field ) :
+		$icon = $field['icon'] ?? '';
+		sk_form_input( [
+			'type'    => 'url',
+			'name'    => "settings[social][{$key}]",
+			'id'      => "settings[social][{$key}]",
+			'value'   => $profile_info['social'][ $key ] ?? '',
+			'label'   => $field['title'] ?? '',
+			'placeholder' => 'http://',
+			'prefix'  => '<span class="sk-input-group-addon"><i class="fab fa-' . esc_attr( $icon ) . '"></i></span>',
+		] );
+	endforeach; ?>
 
-    <?php
-    /**
-     */
-    do_action( 'sk_profile_settings_form_bottom', $current_user, $profile_info ); ?>
+	<?php do_action( 'sk_profile_settings_form_bottom', $current_user, $profile_info ); ?>
 
-    <div class="sk-form-group">
-        <div class="sk-w4 ajax_prev sk-text-left" style="margin-left:24%;">
-            <input type="submit" name="sk_update_profile_settings" class="sk-btn sk-btn-danger sk-btn-theme" value="<?php esc_attr_e( 'Update Settings', 'sk' ); ?>">
-        </div>
-    </div>
-
+	<div class="sk-form-group">
+		<div class="sk-w4 ajax_prev sk-text-left" style="margin-left:24%;">
+			<input type="submit" name="sk_update_profile_settings" class="sk-btn sk-btn-danger sk-btn-theme" value="<?php esc_attr_e( 'Update Settings', 'sk' ); ?>">
+		</div>
+	</div>
 </form>
 
-<?php
-/**
- */
-do_action( 'sk_profile_settings_after_form', $current_user, $profile_info ); ?>
-<!--settings updated content end-->
+<?php do_action( 'sk_profile_settings_after_form', $current_user, $profile_info ); ?>
