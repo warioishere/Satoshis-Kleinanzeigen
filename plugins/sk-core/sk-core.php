8f972dea2eb6bdaba92d1130d8a9d6e9403c8eef
warioishere <mario.hofmann@yourdevice.ch>
2026-08-17 18:33:20 +0200

    Bump auf 4.1.38

--- plugins/sk-core/sk-core.php
+++ plugins/sk-core/sk-core.php
@@ -3,7 +3,7 @@
  * Plugin Name: SK Core
  * Plugin URI: https://satoshiskleinanzeigen.space/
  * Description: A private multivendor marketplace plugin for WordPress.
- * Version: 4.1.37
+ * Version: 4.1.38
  * Author: SK
  * Author URI: https://satoshiskleinanzeigen.space/
  * Text Domain: sk-core
